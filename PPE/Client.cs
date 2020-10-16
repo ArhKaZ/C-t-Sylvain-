ae2b86474085be26c406ca86be83b5dda9b15e2f
Sylvain <sylvain.arhkaz@gmail.com>
2020-10-16 16:57:38 +0200

    Ajoutez des fichiers projet.

---
+++ PPE/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE
+{
+    class Client
+    {
+        public int id { get; set; }
+        public string prenom { get; set; }
+        public string nom { get; set; }
+        public int telephone { get; set; }
+        public string mail { get; set; }
+        public int credit { get; set; }
+        public DateTime dateNaissance { get; set; }
+        public string photo { get; set; }
+        public int nbpartie { get; set; }
+    }
+}
---
+++ PPE/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PPE;
+
+namespace PPE
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //Finir le tuto : https://www.c-sharpcorner.com/UploadFile/mahesh/datagrid-in-wpf/#:~:text=WPF%20DataGrid.,display%20data%20from%20a%20collection.&text=In%20this%20article%2C%20you%20will,load%20data%20from%20a%20collection
+            InitializeComponent();
+            DG_Client.ItemsSource = LoadCollectionData();
+        }
+        private List<Client> LoadCollectionData()
+        {
+            List<Client> clients = new List<Client>();
+            clients.Add(new Client()
+            {
+                id = 1,
+                prenom = "Sylvain",
+                nom = "Luiset",
+                telephone = 0622238704,
+                mail = "sylvain.luiset@saintmichelannecy.fr",
+                credit = 1000000,
+                dateNaissance = new DateTime(2000 - 04 - 11),
+                photo = "",
+                nbpartie = 3000
+            });
+            clients.Add(new Client()
+            {
+                id = 1,
+                prenom = "Antoine",
+                nom = "Girard",
+                telephone = 0646824973,
+                mail = "antoine.girard@saintmichelannecy.fr",
+                credit = 300,
+                dateNaissance = new DateTime(2000 - 04 - 11),
+                photo = "",
+                nbpartie = 2
+            });
+            return clients;
+        }
+    }
+}
